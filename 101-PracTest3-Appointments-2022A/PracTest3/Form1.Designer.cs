9839034cf0717511b594e9b75228a6b570a551c3
SamuelWLee27 <137744681+SamuelWLee27@users.noreply.github.com>
2023-07-18 10:55:01 +1200

    Add files via upload

---
+++ 101-PracTest3-Appointments-2022A/PracTest3/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace PracTest3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxCalendar = new System.Windows.Forms.PictureBox();
+            this.listBoxCalendar = new System.Windows.Forms.ListBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fIleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.countAppointmentsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBoxCount = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCalendar)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBoxCalendar
+            // 
+            this.pictureBoxCalendar.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxCalendar.Location = new System.Drawing.Point(11, 49);
+            this.pictureBoxCalendar.Name = "pictureBoxCalendar";
+            this.pictureBoxCalendar.Size = new System.Drawing.Size(500, 600);
+            this.pictureBoxCalendar.TabIndex = 0;
+            this.pictureBoxCalendar.TabStop = false;
+            // 
+            // listBoxCalendar
+            // 
+            this.listBoxCalendar.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBoxCalendar.FormattingEnabled = true;
+            this.listBoxCalendar.ItemHeight = 16;
+            this.listBoxCalendar.Location = new System.Drawing.Point(517, 49);
+            this.listBoxCalendar.Name = "listBoxCalendar";
+            this.listBoxCalendar.Size = new System.Drawing.Size(336, 596);
+            this.listBoxCalendar.TabIndex = 2;
+            this.listBoxCalendar.SelectedIndexChanged += new System.EventHandler(this.listBoxCalendar_SelectedIndexChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fIleToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(865, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fIleToolStripMenuItem
+            // 
+            this.fIleToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFileToolStripMenuItem,
+            this.countAppointmentsToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.exitToolStripMenuItem});
+            this.fIleToolStripMenuItem.Name = "fIleToolStripMenuItem";
+            this.fIleToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fIleToolStripMenuItem.Text = "File";
+            // 
+            // openFileToolStripMenuItem
+            // 
+            this.openFileToolStripMenuItem.Name = "openFileToolStripMenuItem";
+            this.openFileToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.openFileToolStripMenuItem.Size = new System.Drawing.Size(267, 22);
+            this.openFileToolStripMenuItem.Text = "Open file...";
+            this.openFileToolStripMenuItem.Click += new System.EventHandler(this.openFileToolStripMenuItem_Click);
+            // 
+            // countAppointmentsToolStripMenuItem
+            // 
+            this.countAppointmentsToolStripMenuItem.Name = "countAppointmentsToolStripMenuItem";
+            this.countAppointmentsToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
+            | System.Windows.Forms.Keys.C)));
+            this.countAppointmentsToolStripMenuItem.Size = new System.Drawing.Size(267, 22);
+            this.countAppointmentsToolStripMenuItem.Text = "Count appointments...";
+            this.countAppointmentsToolStripMenuItem.Click += new System.EventHandler(this.countAppointmentsToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(264, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.E)));
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(267, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // textBoxCount
+            // 
+            this.textBoxCount.Location = new System.Drawing.Point(478, 28);
+            this.textBoxCount.Name = "textBoxCount";
+            this.textBoxCount.Size = new System.Drawing.Size(100, 20);
+            this.textBoxCount.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(335, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Appointment type for count:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(865, 723);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxCount);
+            this.Controls.Add(this.listBoxCalendar);
+            this.Controls.Add(this.pictureBoxCalendar);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "101 Practical Test 3";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCalendar)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxCalendar;
+        private System.Windows.Forms.ListBox listBoxCalendar;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fIleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem countAppointmentsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBoxCount;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ 101-PracTest3-Appointments-2022A/PracTest3/Form1.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PracTest3
+{
+    public partial class Form1 : Form
+    {
+        //Name: Samuel Lee
+        //ID: 1595395
+
+        //The number of days in the calendar, 1 is Monday and 5 is Friday
+        const int NUM_DAYS = 5;
+        //The number of hours to display for each day
+        const int NUM_HOURS = 10;
+        //The starting hour for the calendar (9am)
+        const int START_HOUR = 9;
+        //The amount of gap between the text and outline of an appointment
+        const int OFFSET = 2;
+        //The width of an appointment
+        const int APP_WIDTH = 100;
+        //The height of an appointment
+        const int APP_HEIGHT = 60;
+        //Create lists
+        List<string> dayList = new List<string>();
+        List<int> hoursList = new List<int>();
+        List<string> appTypeList = new List<string>();
+        List<string> textList = new List<string>();
+
+        //Colour variables for the different appointments
+        Color WORK_COLOUR = Color.Gold;
+        Color PERSONAL_COLOUR = Color.LightBlue;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Draws the given text at the specified x and y coordinates
+        /// </summary>
+        /// <param name="paper">Where to draw the text</param>
+        /// <param name="hour">The hour of the appointment</param>
+        /// <param name="text">The text of the appointment</param>
+        /// <param name="x">The x position of the appointment</param>
+        /// <param name="y">The y position of the appointment</param>
+        private void DrawText(Graphics paper, int hour, string text, int x, int y)
+        {
+            paper.DrawString(hour.ToString() + ".00: " + text, new Font("Arial", 8),
+                Brushes.Black, x + OFFSET, y + OFFSET);
+
+        }
+
+        /// <summary>
+        /// Draws an appointment in the given color.
+        /// </summary>
+        /// <param name="paper">Where to draw the appointment</param>
+        /// <param name="x">The x position of the appointment</param>
+        /// <param name="y">The y position of the appointment</param>
+        /// <param name="backColor">The background color of the appointment</param>
+        private void DrawAppointment(Graphics paper, int x, int y, Color backColor)
+        {
+            SolidBrush br = new SolidBrush(backColor);
+            Pen pen1 = new Pen(Color.Black, 1);
+
+            paper.FillRectangle(br, x, y, APP_WIDTH, APP_HEIGHT);
+            paper.DrawRectangle(pen1, x, y, APP_WIDTH, APP_HEIGHT);
+        }
+
+        /// <summary>
+        /// Draws an empty calendar.
+        /// </summary>
+        /// <param name="paper">Where to draw the calendar</param>
+        private void DrawEmptyCalendar(Graphics paper)
+        {
+            //The x position of the current appointment
+            int x = 0;
+            //The y position of the current appointment
+            int y = 0;
+
+            //Clear the drawing area
+            pictureBoxCalendar.Refresh();
+
+            //Loop for each day in the calendar
+            for (int hour = 1; hour <= NUM_HOURS; hour++)
+            {
+                //Loop for each hour in the calendar
+                for (int day = 1; day <= NUM_DAYS; day++)
+                {
+                    //Draw the appointment and then move to the next position
+                    DrawAppointment(paper, x, y, Color.White);
+                    x += APP_WIDTH;
+                }
+
+                //Move down to the next row and move back to beginning of the row
+                y += APP_HEIGHT;
+                x = 0;
+            }
+        }
+
+        /// <summary>
+        /// Calculate the x position of an appointment.
+        /// </summary>
+        /// <param name="day">The day of the appointment</param>
+        /// <returns>The x position of the appointment based on the given day</returns>
+        private int CalculateX(string day)
+        {
+            //Stores all the days to display on the calendar
+            string[] dayArray = new string[] { "Mon", "Tue", "Wed", "Thu", "Fri" };
+            //The x position of the appointment
+            int x = 0;
+            //The number for a day, Mon = 0, Fri = 4
+            int dayNum = 0;
+
+            //Look through the array for the given day
+            for (int i = 0; i < dayArray.Length; i++)
+            {
+                //Check if the current day matches the given day
+                if (dayArray[i] == day)
+                {
+                    //Store the day number for the given day
+                    dayNum = i;
+                }
+            }
+
+            //Calculate the x position of the appointment depending on the day
+            x = dayNum * APP_WIDTH;
+
+            return x;
+        }
+        /// <summary>
+        /// calculatr y position
+        /// </summary>
+        /// <param name="hour">Hour of the appointment</param>
+        /// <returns></returns>
+        private int CalculateY(int hour)
+        {
+            //declear varible
+            int y = 0;
+            //caculate y
+            y = (hour - START_HOUR) * APP_HEIGHT;
+            //return y
+            return y;
+        }
+        /// <summary>
+        /// when selected draw red appointment
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void listBoxCalendar_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //create graphics paper
+            Graphics paper = pictureBoxCalendar.CreateGraphics();
+            //declear index clicked
+            int index = listBoxCalendar.SelectedIndex;
+            //declear varibles
+            string day = dayList[index];
+            int hour = hoursList[index];
+            string text = textList[index];
+            
+            //call Calculate x method
+            int xPos = CalculateX(day);
+            //call callculate y method
+            int yPos = CalculateY(hour);
+            //call draw app menthod
+            DrawAppointment(paper, xPos, yPos, Color.Red);
+            //call draw text method
+            DrawText(paper, hour, text, xPos, yPos);
+
+        }
+        /// <summary>
+        /// count number of appointments that have the same type as in the textBox then display in textBox
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void countAppointmentsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //declear varible
+            int numApps = 0;
+            //get user input from text
+            string userAppType = textBoxCount.Text;
+            //search list
+            for (int i = 0; i < appTypeList.Count; i++)
+            {
+                //check if string at i position is the same as user input
+                if (appTypeList[i] == userAppType)
+                {
+                    //add one to counter
+                    numApps++;
+                }
+            }
+            //diplay how many appointment with same type as userinput there are
+            MessageBox.Show("Number of appointment with " + userAppType + " type: " + numApps.ToString());
+        }
+        /// <summary>
+        /// Exit application
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        /// <summary>
+        /// open csv file and read then display info 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void openFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //declear varibles
+            string line = "";
+            string day = "";
+            int hour = 0;
+            string appointmentType = "";
+            string textToDisplay = "";
+            int xPos = 0;
+            int yPos = 0;
+            int app = 0;
+            //declear reader
+            StreamReader reader;
+            //create picturebox
+            Graphics paper = pictureBoxCalendar.CreateGraphics();
+            //filter for files
+            const string FILTER = "CSV Files|*.csv|All Files|*.*";
+            //Set filter for dialog control
+            openFileDialog1.Filter = FILTER;
+            //check if user selected file
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //call drawEmptyCalender method
+                DrawEmptyCalendar(paper);
+                //open file
+                reader = File.OpenText(openFileDialog1.FileName);
+                //loop until end of file
+                while (!reader.EndOfStream)
+                {
+                    try
+                    {
+                        //read line to varible
+                        line = reader.ReadLine();
+                        //split line by , into an array
+                        string[] CSVarray = line.Split(',');
+                        //check if array is right length
+                        if (CSVarray.Length == 4)
+                        {
+                            //put data in varibles
+                            day = CSVarray[0];
+                            hour = int.Parse(CSVarray[1]);
+                            appointmentType = CSVarray[2];
+                            textToDisplay = CSVarray[3];
+                            //display in listBox
+                            listBoxCalendar.Items.Add(day.PadRight(8) + hour.ToString().PadRight(8) + appointmentType.PadRight(10) + textToDisplay);
+                            dayList.Add(day);
+                            hoursList.Add(hour);
+                            appTypeList.Add(appointmentType);
+                            textList.Add(textToDisplay);
+                            //call Calculate x method
+                            xPos = CalculateX(day);
+                            //call callculate y method
+                            yPos = CalculateY(hour);
+                            //check type of appointment
+                            if (appointmentType == "Work")
+                            {
+                                //call draw appointment with work colour
+                                DrawAppointment(paper, xPos, yPos, WORK_COLOUR);
+                            }
+                            else if (appointmentType == "Personal")
+                            {
+                                //call drawAppointment method using personal colour
+                                DrawAppointment(paper, xPos, yPos, PERSONAL_COLOUR);
+                            }
+                            //call draw text method
+                            DrawText(paper, hour, textToDisplay, xPos, yPos);
+                            //add one to the number of appointments
+                            app++;
+                        }
+                        else
+                        {
+                            //display error message
+                            Console.WriteLine("Error: " + line);
+                        }
+                    }
+                    catch 
+                    {
+                        //display error message
+                        Console.WriteLine("Error: " + line);
+                    }
+                }
+                //close file
+                reader.Close();
+                //display number of appointments
+                MessageBox.Show("Number of appointments: " + app);
+            }
+        }
+    }
+}
