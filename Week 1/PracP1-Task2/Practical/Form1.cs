6b1a0fcbcd5b8ddba986ff918bbf858e08e148f9
SamuelWLee27 <137744681+SamuelWLee27@users.noreply.github.com>
2023-07-18 11:07:34 +1200

    Add files via upload

---
+++ Week 1/PracP1-Task2/Practical/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practical
+{
+    public partial class Form1 : Form
+    {
+        //An array that stores rain fall values in mm
+        int[] rainfallArray = new int[] { 10, 5, 30, 50, 0, 2, 0, 0, 8, 25, 15, 6, 0, 0, 22 };
+        //The width of a bar in the graph
+        const int BAR_WIDTH = 40;
+        //The maximum amount of rainfall
+        const int MAX_RAINFALL = 100;
+        //The gap between the bars on the graph
+        const int GAP = 5;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Calculates the barheight for a rainfall value.
+        /// </summary>
+        /// <param name="value">The rainfall value to calculate the bar height for</param>
+        /// <returns>The height of a bar in the graph for the rainfall value</returns>
+        private int CalcBarHeight(int value)
+        {
+            int height = pictureBoxDisplay.Height * value / MAX_RAINFALL;
+            return height;
+        }
+
+        /// <summary>
+        /// Closes the application.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        /// <summary>
+        /// Clears the picturebox.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            pictureBoxDisplay.Refresh();
+        }
+        /// <summary>
+        /// Calculate number of days with zero rain once clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonCalc_Click(object sender, EventArgs e)
+        {
+            //declear varible
+            int counter = 0;
+            //loop through array
+            for(int i = 0; i < rainfallArray.Length; i++)
+            {
+                //check if amount of rain is equal to zero
+                if (rainfallArray[i] == 0)
+                {
+                    //add one to counter
+                    counter++;
+                }
+            }
+            //display number of days with no rain
+            MessageBox.Show(counter + " Days of rain");
+        }
+        /// <summary>
+        /// Draw graph for amount of rain once clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonDrawGraph_Click(object sender, EventArgs e)
+        {
+            //declear varibles
+            Graphics paper = pictureBoxDisplay.CreateGraphics();
+            int height = 0;
+            SolidBrush br = new SolidBrush(Color.LightBlue);
+            int xPos = 0;
+            int yPos = 0;
+            //loop through array
+            for(int i = 0;i < rainfallArray.Length; i++)
+            {
+                //get bar height
+                height = CalcBarHeight(rainfallArray[i]);
+                //set y so bar rises from bottom
+                yPos = pictureBoxDisplay.Height - height;
+                //Draw bar
+                paper.FillRectangle(br, xPos, yPos, BAR_WIDTH, height);
+                //Shift x position
+                xPos += BAR_WIDTH + GAP;
+            }
+
+        }
+    }
+}
---
+++ Week 1/PracP1-Task3/PracP1-Task3/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PracP1_Task3
+{
+    public partial class Form1 : Form
+    {
+        //Declear lists
+        List<int> votesList = new List<int>();
+        List<string> partyList = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// open file and calculate total votes and number of seat for each party
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void openFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //filter for files
+            const string FILTER = "CSV Files|*.csv|All Files|*.*";
+            //declear veriables
+            StreamReader reader;
+            string line = "";
+            string[] csvArray;
+            string party = "";
+            int votes = 0;
+            int totalVotes = 0;
+            int seats = 0;
+            int seatsPerParty = 0;
+            //set filter for dialog control
+            openFileDialog1.Filter = FILTER;
+            //check if user selected file
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //open file
+                reader = File.OpenText(openFileDialog1.FileName);
+                //loop through to end of file
+                while (!reader.EndOfStream)
+                {
+                    try
+                    {
+                        //read line to varible
+                        line = reader.ReadLine();
+                        //split line into array
+                        csvArray = line.Split(',');
+                        //check if array is right length
+                        if(csvArray.Length == 2)
+                        {
+                            //store data in varibles
+                            party = csvArray[0];
+                            votes = int.Parse(csvArray[1]);
+                            //Get total votes
+                            totalVotes += votes;
+                            //add datat to lists
+                            partyList.Add(party);
+                            votesList.Add(votes);
+                        }
+                        else 
+                        {
+                            //error
+                            Console.WriteLine("Error: " + line);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        //error
+                        Console.WriteLine("Error: " + line);
+                    }
+                }
+                //close file
+                reader.Close();
+                //Display total number of votes
+                Console.WriteLine(totalVotes + " total votes");
+                //get number of seats
+                seats = totalVotes / 120;
+                //loop through lists
+                for(int i = 0; i < votesList.Count; i++)
+                {
+                    //calculate seats for each party
+                    seatsPerParty = votesList[i]/seats;
+                    //Dispaly seat for each party to the console
+                    Console.WriteLine(partyList[i] + " has " + seatsPerParty + " seats.");
+                }
+
+            }
+        }
+    }
+}
