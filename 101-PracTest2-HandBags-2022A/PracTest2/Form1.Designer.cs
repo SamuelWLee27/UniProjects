7650d3adbf4b137a7857f0a1782a89f964f156aa
SamuelWLee27 <137744681+SamuelWLee27@users.noreply.github.com>
2023-07-18 10:53:52 +1200

    Add files via upload

---
+++ 101-PracTest2-HandBags-2022A/PracTest2/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace PracTest2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxDisplay = new System.Windows.Forms.PictureBox();
+            this.textBoxDraw = new System.Windows.Forms.TextBox();
+            this.buttonDrawBags = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDisplay)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxDisplay
+            // 
+            this.pictureBoxDisplay.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxDisplay.Location = new System.Drawing.Point(12, 12);
+            this.pictureBoxDisplay.Name = "pictureBoxDisplay";
+            this.pictureBoxDisplay.Size = new System.Drawing.Size(600, 600);
+            this.pictureBoxDisplay.TabIndex = 0;
+            this.pictureBoxDisplay.TabStop = false;
+            // 
+            // textBoxDraw
+            // 
+            this.textBoxDraw.Location = new System.Drawing.Point(627, 12);
+            this.textBoxDraw.Name = "textBoxDraw";
+            this.textBoxDraw.Size = new System.Drawing.Size(100, 20);
+            this.textBoxDraw.TabIndex = 1;
+            // 
+            // buttonDrawBags
+            // 
+            this.buttonDrawBags.Location = new System.Drawing.Point(627, 50);
+            this.buttonDrawBags.Name = "buttonDrawBags";
+            this.buttonDrawBags.Size = new System.Drawing.Size(100, 23);
+            this.buttonDrawBags.TabIndex = 2;
+            this.buttonDrawBags.Text = "Draw Hand Bags";
+            this.buttonDrawBags.UseVisualStyleBackColor = true;
+            this.buttonDrawBags.Click += new System.EventHandler(this.buttonDrawBags_Click);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(627, 150);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(100, 23);
+            this.buttonExit.TabIndex = 3;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(627, 98);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(100, 23);
+            this.buttonClear.TabIndex = 4;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(739, 624);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonDrawBags);
+            this.Controls.Add(this.textBoxDraw);
+            this.Controls.Add(this.pictureBoxDisplay);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "101 Practical Test 2";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDisplay)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxDisplay;
+        private System.Windows.Forms.TextBox textBoxDraw;
+        private System.Windows.Forms.Button buttonDrawBags;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Button buttonClear;
+    }
+}
+
---
+++ 101-PracTest2-HandBags-2022A/PracTest2/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PracTest2
+{
+    public partial class Form1 : Form
+    {
+        //Name:Samuel Lee
+        //ID:1595395
+
+        //Width of a hand bag
+        const int BAG_WIDTH = 50;
+        //Height of a hand bag
+        const int BAG_HEIGHT = 50;
+        //The number of columns of hand bags to draw
+        const int NUM_COLUMNS = 10;
+        //The gap between rows and columns
+        const int GAP = 10;
+        //Number of columns
+        const int COL = 10;
+        //max hand bags
+        const int MAX_HAND_BAGS = 10;
+        //min hand bags
+        const int MIN_HAND_BAGS = 2;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void ClearTextBox()
+        {
+            //Clear textBox
+            textBoxDraw.Clear();
+            //focus textbox
+            textBoxDraw.Focus();
+        }
+        /// <summary>
+        /// Draw hand bags
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonDrawBags_Click(object sender, EventArgs e)
+        {
+            //Declear starting x and y pos
+            int x = 0;
+            int y = 0;
+            //Create graphics paper
+            Graphics paper = pictureBoxDisplay.CreateGraphics();
+            //Make pen
+            Pen pen1 = new Pen(Color.Black, 2);
+            //Make Brush
+            SolidBrush br = new SolidBrush(Color.Red);
+            try
+            {
+                //get user input
+                int handBags = int.Parse(textBoxDraw.Text);
+                //Check if number of handbags is valid
+                if (handBags >= MIN_HAND_BAGS && handBags <= MAX_HAND_BAGS)
+                {
+                    //loop for rows needed
+                    for (int rows = 1; rows <= handBags; rows++)
+                    {
+                        //loop for columns
+                        for (int numCol = 1; numCol <= COL; numCol++)
+                        {
+                            //Check if colunm number is a multiple of 3
+                            if (numCol % 3 == 0)
+                            {
+                                //Change square to purple
+                                br.Color = Color.Purple;
+                            }
+                            //if not multiple of 3
+                            else
+                            {
+                                //Change square to red
+                                br.Color = Color.Red;
+                            }
+                            //Draw square at current x and y
+                            paper.FillRectangle(br, x, y, BAG_WIDTH, BAG_HEIGHT);
+                            paper.DrawRectangle(pen1, x, y, BAG_WIDTH, BAG_HEIGHT);
+                            //shift x by bag width and gap
+                            x += BAG_WIDTH + GAP;
+                        }
+                        //Set x to orignal place
+                        x = 0;
+                        //shift y by bag height and gap
+                        y += BAG_HEIGHT + GAP;
+                    }
+                }
+                else
+                {
+                    //Error message
+                    MessageBox.Show("Number of bags must be between 2 and 10 inclusive");
+                    //Clear textBox
+                    //textBoxDraw.Clear();
+                    //Clear picturebox
+                    pictureBoxDisplay.Refresh();
+                    //focus textbox
+                    //textBoxDraw.Focus();
+                    //call clear textbox method
+                    ClearTextBox();
+                }
+            }
+            catch (Exception ex)
+            {
+                //Error message
+                MessageBox.Show(ex.Message);
+                //Clear textBox
+                //textBoxDraw.Clear();
+                //Clear picturebox
+                pictureBoxDisplay.Refresh();
+                //focus textbox
+                //textBoxDraw.Focus();
+                //call clear textbox method
+                ClearTextBox();
+            }
+        }
+
+        /// <summary>
+        /// Clear textbox and picture box and focus textbox
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            //Clear textBox
+            //textBoxDraw.Clear();
+            //Clear picturebox
+            pictureBoxDisplay.Refresh();
+            //focus textbox
+            //textBoxDraw.Focus();
+            //call clear textbox method
+            ClearTextBox();
+        }
+
+        /// <summary>
+        /// Exit application
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
