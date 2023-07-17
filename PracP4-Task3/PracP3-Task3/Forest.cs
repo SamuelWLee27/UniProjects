7e5435b23453cdb787a184d2d68cc4681f3ef1b3
SamuelWLee27 <137744681+SamuelWLee27@users.noreply.github.com>
2023-07-18 11:17:47 +1200

    Add files via upload

---
+++ PracP4-Task3/PracP3-Task3/Forest.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.IO;
+
+namespace PracP3_Task3
+{
+    public class Forest
+    {
+        private Random _rand;
+        private List<Tree> _forestList;
+        /// <summary>
+        /// Initialises the object to the values passed in.
+        /// </summary>
+        public Forest()
+        {
+            _rand = new Random();
+            _forestList = new List<Tree>();
+        }
+        /// <summary>
+        /// adds tree to list
+        /// </summary>
+        /// <param name="x"> x pos of tree</param>
+        /// <param name="y">y pos of tree</param>
+        /// <param name="size">size of tree</param>
+        public void AddTree(int x, int y, int size)
+        {
+            _forestList.Add(new Tree(x, y, size));
+        }
+        /// <summary>
+        /// Draws trees in forest
+        /// </summary>
+        /// <param name="paper"> canvas drawing on</param>
+        public void DrawTrees(Graphics paper)
+        {
+            
+            foreach(Tree t in _forestList)
+            {
+                t.DrawTree(paper);
+            }
+        }
+        /// <summary>
+        /// save forest trees to file
+        /// </summary>
+        /// <param name="filename"></param>
+        public void SaveFile(string filename)
+        {
+            StreamWriter writer;
+            //create file
+            writer = File.CreateText(filename);
+
+            foreach(Tree t in _forestList)
+            {
+                writer.WriteLine(t.ToCsvString());
+            }
+
+
+            writer.Close();
+        }
+        /// <summary>
+        /// opens files and adds trees to list
+        /// </summary>
+        /// <param name="filename"></param>
+        public void OpenFile(string filename)
+        {
+            string line = "";
+            StreamReader reader;
+            
+            //open file
+            reader = File.OpenText(filename);
+           
+            while (!reader.EndOfStream)
+            {
+                try
+                {
+                    line = reader.ReadLine();
+                    string[] csvArray = line.Split(',');
+                    //add tree to list
+                    _forestList.Add(new Tree(int.Parse(csvArray[0]), int.Parse(csvArray[1]), int.Parse(csvArray[2])));
+                }
+                catch 
+                {
+                    Console.WriteLine("Error: " + line);
+                }
+            }
+
+            reader.Close();
+        }
+        /// <summary>
+        /// makes random tree grow
+        /// </summary>
+        /// <param name="growth"> size tree grows by</param>
+        public void GrowForest(int growth)
+        {
+            //check if there is a tree in tree list
+            if (!(_forestList.Count == 0))
+            {
+                int index = _rand.Next(0, _forestList.Count);
+                //get tree at random index
+                Tree t = _forestList[index];
+                //Grow tree
+                t.GrowTree(growth);
+                //remove tree then replace by grown tree
+                _forestList.RemoveAt(index);
+                _forestList.Insert(index, t);
+
+            }
+        }
+    }
+}
---
+++ PracP4-Task3/PracP3-Task3/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PracP3_Task3
+{
+    public partial class Form1 : Form
+    {
+        //filter for files
+        const string FILTER = "CSV Files|*.csv|All Files|*.*";
+        ////list
+        //List<Tree> treesList = new List<Tree>();
+        Forest forest = new Forest();
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Enabled = true;
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            //clear picture box
+            e.Graphics.Clear(Color.LightGray);
+
+            //draw trees
+            forest.DrawTrees(e.Graphics);
+
+
+            ////loop through tree list and draw trees
+            //    foreach (Tree tree in treesList)
+            //    {
+            //        tree.DrawTree(e.Graphics);
+            //    }
+         
+        }
+        /// <summary>
+        /// timer refresh picture box
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            forest.GrowForest(2);
+            pictureBox1.Refresh();
+        }
+
+        /// <summary>
+        /// Adds tree where clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            try 
+            {
+                forest.AddTree(e.X, e.Y, int.Parse(textBoxFoliage.Text));
+                ////add tree to list where clicked
+                //treesList.Add(new Tree(e.X, e.Y, int.Parse(textBoxFoliage.Text)));
+
+            }
+            catch (Exception ex)
+            {
+                //error
+                MessageBox.Show(ex.Message);
+            }
+}
+        /// <summary>
+        /// exits program
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        /// <summary>
+        /// open file with trees and draw them
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void openFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //set filter for dialog control
+            openFileDialog1.Filter = FILTER;
+            //check if user selected file
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string filename = openFileDialog1.FileName;
+                forest.OpenFile(filename);
+            }
+        }
+        /// <summary>
+        /// saves trees to file in csv formate
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void saveFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Set filter for dialog control
+            saveFileDialog1.Filter = FILTER;
+            //check if user selected file
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string filename = saveFileDialog1.FileName;
+                forest.SaveFile(filename);
+            }
+        }
+    }
+}
