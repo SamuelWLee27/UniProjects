2f79963a63d7eb07542c42fdb003e6f7f0f9cb96
SamuelWLee27 <137744681+SamuelWLee27@users.noreply.github.com>
2023-08-20 12:27:17 +1200

    Add files via upload

---
+++ Week3-E2/Week3-E2/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Week3_E2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonTriangle = new System.Windows.Forms.Button();
+            this.buttonSquare = new System.Windows.Forms.Button();
+            this.pictureBoxDisplay = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDisplay)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonTriangle
+            // 
+            this.buttonTriangle.Location = new System.Drawing.Point(136, 342);
+            this.buttonTriangle.Name = "buttonTriangle";
+            this.buttonTriangle.Size = new System.Drawing.Size(128, 23);
+            this.buttonTriangle.TabIndex = 0;
+            this.buttonTriangle.Text = "Draw a triangle";
+            this.buttonTriangle.UseVisualStyleBackColor = true;
+            this.buttonTriangle.Click += new System.EventHandler(this.buttonTriangle_Click);
+            // 
+            // buttonSquare
+            // 
+            this.buttonSquare.Location = new System.Drawing.Point(530, 342);
+            this.buttonSquare.Name = "buttonSquare";
+            this.buttonSquare.Size = new System.Drawing.Size(110, 23);
+            this.buttonSquare.TabIndex = 1;
+            this.buttonSquare.Text = "Draw a square";
+            this.buttonSquare.UseVisualStyleBackColor = true;
+            // 
+            // pictureBoxDisplay
+            // 
+            this.pictureBoxDisplay.Location = new System.Drawing.Point(136, 42);
+            this.pictureBoxDisplay.Name = "pictureBoxDisplay";
+            this.pictureBoxDisplay.Size = new System.Drawing.Size(504, 294);
+            this.pictureBoxDisplay.TabIndex = 2;
+            this.pictureBoxDisplay.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBoxDisplay);
+            this.Controls.Add(this.buttonSquare);
+            this.Controls.Add(this.buttonTriangle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDisplay)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonTriangle;
+        private System.Windows.Forms.Button buttonSquare;
+        private System.Windows.Forms.PictureBox pictureBoxDisplay;
+    }
+}
+
